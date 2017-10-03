a6aef33c54915cad9515f7ca92559286f7f5ad1e
Dhika Bagas <31851125+bagasdhika@users.noreply.github.com>
2017-10-03 08:30:23 +0700

    Update hitungvektor.php

--- hitungvektor.php
+++ hitungvektor.php
@@ -1,49 +1,33 @@
 <html>
-<head>
-	<title>Hitung Vektor</title>
-<body align="center" style="background-color: pink">
+<title>Aplikasi Simpan</title>
+<body align="center" style="background-color: rose">
 <center><img src="img3.png" style="width:900px;height:150px;"><br>
 <br>
-<h2>Hitung Vektor</h2>
+<h2>Simpan PDF<h2>
 <?php
-$host='localhost';
-$user='root';
-$pass='';
-$database='dbstbi';
+// Tentukan folder file yang boleh di download
+$folder = "files/";
+// Lalu cek menggunakan fungsi file_exist
+if (!file_exists($folder.$_GET['file'])) {
+  echo "<h1>Access forbidden!</h1>
+      <p> Anda tidak diperbolehkan mendownload file ini.</p>";
+  exit;
+}
 
-$conn=mysql_connect($host,$user,$pass);
-mysql_select_db($database);
-//hitung index
-mysql_query("TRUNCATE TABLE tbvektor");
-//ambil setiap DocId dalam tbindex
-	//hitung panjang vektor untuk setiap DocId tersebut
-	//simpan ke dalam tabel tbvektor
-	$resDocId = mysql_query("SELECT DISTINCT DocId FROM tbindex");
-	
-	$num_rows = mysql_num_rows($resDocId);
-	print("Terdapat " . $num_rows . " dokumen yang dihitung panjang vektornya. <br />");
-	
-	while($rowDocId = mysql_fetch_array($resDocId)) {
-		$docId = $rowDocId['DocId'];
-	
-		$resVektor = mysql_query("SELECT Bobot FROM tbindex WHERE DocId = '$docId'");
-		
-		//jumlahkan semua bobot kuadrat 
-		$panjangVektor = 0;		
-		while($rowVektor = mysql_fetch_array($resVektor)) {
-			$panjangVektor = $panjangVektor + $rowVektor['Bobot']  *  $rowVektor['Bobot'];	
-		}
-		
-		//hitung akarnya		
-		$panjangVektor = sqrt($panjangVektor);
-		
-		//masukkan ke dalam tbvektor		
-		$resInsertVektor = mysql_query("INSERT INTO tbvektor (DocId, Panjang) VALUES ('$docId', $panjangVektor)");	
-	} //end while $rowDocId
-
-?>
-<br>
-<br>
-<a href="index.php"><input type="button" value="<< Kembali"/></a>
+// Apabila mendownload file di folder files
+else {
+  header("Content-Type: octet/stream");
+  header("Content-Disposition: attachment; 
+  filename=\"".$_GET['file']."\"");
+  $fp = fopen($folder.$_GET['file'], "r");
+  $data = fread($fp, filesize($folder.$_GET['file']));
+  fclose($fp);
+  print $data;
+}
+?><!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
 </body></center>
 </html>
