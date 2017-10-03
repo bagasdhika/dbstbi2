56c5f961c445e00d9a260b6c5acd6f7758d6f723
Dhika Bagas <31851125+bagasdhika@users.noreply.github.com>
2017-10-03 08:28:52 +0700

    Update hasilquery.php

--- hasilquery.php
+++ hasilquery.php
@@ -1,4 +1,10 @@
- <?php
+<html>
+<title>Aplikasi Download</title>
+<body align="center" style="background-color: cyan">
+<center><img src="img3.png" style="width:900px;height:150px;"><br>
+<br>
+<h2>Hasil Query Boolean</h2>
+<?php
  //https://dev.mysql.com/doc/refman/5.5/en/fulltext-boolean.html
  //ALTER TABLE dokumen
 //ADD FULLTEXT INDEX `FullText` 
@@ -13,16 +19,17 @@ $katakunci="";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
+$conn=mysqli_connect('localhost','root','','dbstbi');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-$hasil=$_POST[katakunci];
-//$sql = "SELECT distinct nama_file,token,tokenstem FROM `dokumen` where token like '%$hasil%'";
-$sql = "SELECT distinct nama_file,token,tokenstem FROM `dokumen` WHERE MATCH (token,tokenstem) AGAINST ('$hasil' IN BOOLEAN MODE)";
+$hasil=$_POST['katakunci'];
+$sql = "SELECT distinct nama_file,token,tokenstem FROM `dokumen` where token like '%$hasil%'";
+//$sql = "SELECT distinct nama_file,token,tokenstem FROM dokumen WHERE match (token,tokenstem) against ('$hasil' IN BOOLEAN MODE);";
 
 
-echo $sql;
-$result = $conn->query($sql);
+ // echo $sql;die();
+$result = mysqli_query($conn,$sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
@@ -37,3 +44,8 @@ $conn->close();
 ///
 
 ?>
+<br>
+<br>
+<a href="index.php"><input type="button" value="<< Kembali"/></a>
+</body></center>
+</html>
