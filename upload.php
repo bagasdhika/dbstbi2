0fe389304c1ec29e810cceb75b0a4704ca219f7b
Dhika Bagas <31851125+bagasdhika@users.noreply.github.com>
2017-10-03 08:28:09 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,22 @@
 <html>
 <title>Form Upload</title>
-<body>
+<body align="center" style="background-color: red">
+<center><img src="img3.png" style="width:900px;height:150px;">
+<br>
+<br>
+<h2>Upload Files</h2>
 <form enctype="multipart/form-data" method="POST" action="hasil_upload.php">
 File yang di upload : <input type="file" name="fupload"><br>
+<br>
 Deskripsi File : <br>
-<textarea name="deskripsi" rows="8" cols="40"></textarea><br>
+<br>
+<textarea name="deskripsi" rows="8" cols="40"></textarea>
+<br>
+<br>
 <input type=submit value=Upload>
+<a href="index.php"><input type="button" value="<< Kembali"/></a>
 </form>
+<br>
+<br><a href="index.php"><input type="button" value="<< Kembali"/></a>
+</body></center>
+</html>
