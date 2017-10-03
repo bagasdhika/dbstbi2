c8ca6457b264789d12ebbfd70d9360b536c0d26e
Dhika Bagas <31851125+bagasdhika@users.noreply.github.com>
2017-10-03 08:23:04 +0700

    Update query.php

--- query.php
+++ query.php
@@ -1,8 +1,14 @@
 <html>
 <title>Query</title>
-<body>
+<body align="center" style="background-color: tomato">
+<center><img src="img3.png" style="width:900px;height:150px;">
+<h2>Query Boolean</h2>
 <form enctype="multipart/form-data" method="POST" action="hasilquery.php">
-Keyword : <br>
-<input type="text" name="katakunci"><br>
+<b>Masukan Keyword : </b><br>
+<br><input type="text" name="katakunci"><br><br>
 <input type=submit>
 </form>
+<a href="index.php"><input type="button" value="<< Kembali"/></a>
+</body>
+</center>
+</html>
