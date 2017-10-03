7f3f4dd58a2538b856df2ad832bf925222071fcc
Dhika Bagas <31851125+bagasdhika@users.noreply.github.com>
2017-10-03 08:25:25 +0700

    Update awalquery.php

--- awalquery.php
+++ awalquery.php
@@ -1,8 +1,14 @@
 <html>
-<title>Form Upload</title>
-<body>
+<title>Awal Query</title>
+<center><body align="center" style="background-color: lime">
+<img src="img3.png" style="width:900px;height:150px;"><br>
+<h2>Awal Query</h2>
 <form enctype="multipart/form-data" method="POST" action="querytf2.php">
 Kata Kunci : <br>
   <input type="text" name="keyword"><br>
+  <br>
 <input type=submit value=Submit>
 </form>
+<a href="index.php"><input type="button" value="<< Kembali"/></a>
+</body></center>
+</html>
