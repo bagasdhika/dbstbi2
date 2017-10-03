791e97667c2e517705c07516d81380e1a6afcfb1
Dhika Bagas <31851125+bagasdhika@users.noreply.github.com>
2017-10-03 08:27:10 +0700

    Update download.php

--- download.php
+++ download.php
@@ -1,15 +1,19 @@
 <html>
 <title>Aplikasi Download</title>
-<body>
+<center><body align="center" style="background-color: silver">
+<img src="img3.png" style="width:900px;height:150px;"><br>
+<br>
+<h2>Download PDF</h2></center>
 <?php
-  $konek = mysqli_connect("localhost","root","","upload");
-
+  $konek = mysqli_connect("localhost","root","","dbstbi");
   $query = "SELECT * FROM upload ORDER BY id_upload DESC";
   $hasil = mysqli_query($konek, $query);
-
   while ($r = mysqli_fetch_array($hasil)){
     echo "Nama File : <b>$r[nama_file]</b> <br>";
     echo "Deskripsi : $r[deskripsi] <br>";
     echo "<a href=\"simpan.php?file=$r[nama_file]\">Download File</a><hr><br>";
   }
 ?>
+<center><a href="index.php"><input type="button" value="<< Kembali"/></a></center>
+</body>
+</html>
